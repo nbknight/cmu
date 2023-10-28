a12236c0dac73111b1da86cce4bb3659b3771226
Nick Knight <nick.knight@riaadvisory.com>
2023-10-28 12:17:53 -0400

    WEEK2 assignment

--- week2/week2_startup.sh
+++ week2/week2_startup.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
+SCRIPTNAME=$0;
+
+# Create folder for server files
+mkdir -p /var/server
+msg 'Server directory created'
+
+# Logging
+logfile="/var/server/${SCRIPTNAME}.log"
+msg () {
+    if [ $? -eq 0 ];then
+        echo "[$(date)] [INFO] $1  executed successfully" >> $logfile
+    else
+        echo "[$(date)] [ERR] $1 failed with exit code $?" >> $logfile
+    fi
+}
 
 # Libraries
-
 # Install Apache2
 apt install apache2 -y
+msg 'Apache installation'
 service apache2 start
+msg 'Start apache'
 # Install PHP Server
 apt update
 apt install php libapache2-mod-php -y
+msg 'PHP installation'
 
 # restart apache2 after installing php
 service apache2 restart
+msg 'Restart Apache after PHP install'
 
-# Is git installed
-
-# Create a folder for organization and move into it
-mkdir -p /home/ubuntu/server
-cd /home/ubuntu/server
+# Install git
+apt install git
+msg 'Git installation'
 
 # clone provided git repo
+cd /var/server
 git clone https://github.com/tsdevopsacp/sample-app.git
-
+msg 'Clone Git repo'
 # move server files to apache2 folder
 cp -r sample-app/* /var/www/html
-
-# Still need to add logging and error handling
+msg 'Webapp files copied to deployment directory'
